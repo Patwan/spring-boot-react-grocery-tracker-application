f4c02c35d8f1c2c479a9dabef39986b0f21bf445
Martin Njogu <mndungu730@gmail.com>
2021-05-04 18:29:13 +0300

    Added Category and User entities

---
+++ src/main/java/com/pwebk/expensetracker/model/Category.java
@@ -0,0 +1,24 @@
+package com.pwebk.expensetracker.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "category")
+public class Category {
+
+    private long id;
+
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+}
